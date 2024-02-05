3e38d3b6e3feafd57298787f512173d579e5389f
Josh <jvargasprog@gmail.com>
2024-02-04 21:29:31 -0800

    removed api key lol

--- CurrentWeather_test.go
+++ CurrentWeather_test.go
@@ -1,12 +1,10 @@
 package openweathergo
 
 import (
-	"os"
 	"testing"
 )
 
 func TestCurrentWeatherZIP(t *testing.T) {
-	os.Setenv("OWKEY", "f0b7c0f3986510cac41a23797f3fb0a5")
 	data := GetCurrentByZIP(94040, "us") //Should get weather from Mountain View CA
 
 	if data.COD != 200 {
@@ -18,7 +16,6 @@ func TestCurrentWeatherZIP(t *testing.T) {
 }
 
 func TestCurrentWeatherCOORD(t *testing.T) {
-	os.Setenv("OWKEY", "f0b7c0f3986510cac41a23797f3fb0a5")
 	data := GetCurrentByCoords(37.38, -122.08)
 	if data.COD != 200 {
 		t.Errorf("Error talking to server, got code %d", data.COD)
